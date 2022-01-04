986ff726fccdddddd6dffb321aa60ea2c0a0c140
JordanCH05 <jordi.caristiahill@gmail.com>
2022-01-04 18:41:07 +0000

    Install libraries

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE.URL"] = "postgres://ibecdjiabompgq:4b5aa61760bfbc8506d250d7d793091472b01c30f2bfe106153f02c84160fc94@ec2-34-253-116-145.eu-west-1.compute.amazonaws.com:5432/d5kaodu2m3lhke"
+os.environ["SECRET_KEY"] = "Ea+ESW?HMs5TWM-@"
