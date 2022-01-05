8fa165b16bd05895ba37c25162843aec8a875ab0
JordanCH05 <jordi.caristiahill@gmail.com>
2022-01-05 18:52:19 +0000

    Add Review and Game models

--- reviewsite/models.py
+++ reviewsite/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Review(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name="name")
+    game = models.CharField(max_length=200)
+    score = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    body = models.TextField(blank=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'Review of {self.game} by {self.username}'
+
+class Game(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    developer = models.CharField(max_length=200)
+    score = models.IntegerField()
+    image = models.ImageField()
+    description = models.TextField()
+    approved = models.BooleanField(default=False)
+    reviews = models.ManyToManyField(Review, related_name='game_reviews', blank=True)
+
+    class Meta:
+        ordering = ['-score']
+
+    def __str__(self):
+        return self.title
+
