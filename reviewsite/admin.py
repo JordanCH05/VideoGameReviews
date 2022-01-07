89bbb9106c64a69e2423c440aacadb45039fa123
JordanCH05 <jordi.caristiahill@gmail.com>
2022-01-07 13:25:36 +0000

    Create admin superuser and register Game model with summernote description

--- reviewsite/admin.py
+++ reviewsite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Game
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(Game)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('description')
