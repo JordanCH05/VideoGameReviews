c8c37b59058b2d29dfa8c3ef21cd22930e58cf42
JordanCH05 <jordi.caristiahill@gmail.com>
2022-01-10 14:35:02 +0000

    deployment commit

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ["DATABASE_URL"] = "postgres://ibecdjiabompgq:4b5aa61760bfbc8506d250d7d793091472b01c30f2bfe106153f02c84160fc94@ec2-34-253-116-145.eu-west-1.compute.amazonaws.com:5432/d5kaodu2m3lhke"
 os.environ["SECRET_KEY"] = "p(>A?tU<33p;YB;{"
+os.environ["CLOUDINARY_URL"] = "cloudinary://489654314668293:2ftxiHv3RKEAxr_DmO5QY3ZUt6k@jordich"
