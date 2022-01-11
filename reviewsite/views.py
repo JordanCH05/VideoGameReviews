9cefcaf09f3c6ebb60c2904c94e8251732a5e4a7
JordanCH05 <jordi.caristiahill@gmail.com>
2022-01-11 14:33:45 +0000

    Add templates and style sheet

--- reviewsite/views.py
+++ reviewsite/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Game
 
-# Create your views here.
+class GameList(generic.ListView):
+    model = Game
+    queryset = Game.objects.filter(status=1).order_by('-score')
+    template_name = 'index.html'
+    paginate_by = 9
