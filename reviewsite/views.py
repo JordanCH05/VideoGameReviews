e9016f1a76ad6e4e5cc45d2c4144ca0db57518a1
JordanCH05 <jordi.caristiahill@gmail.com>
2022-01-14 17:42:26 +0000

    Add GameDetail view

--- reviewsite/views.py
+++ reviewsite/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Game
 
 class GameList(generic.ListView):
@@ -7,3 +7,10 @@ class GameList(generic.ListView):
     queryset = Game.objects.filter(approved=True).order_by('-score')
     template_name = 'index.html'
     paginate_by = 9
+
+class GameDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Game.objects.filter(approved=True)
+        post = get_object_or_404(queryset, slug=slug)
+        reviews = game.reviews.filter(approved=True)
