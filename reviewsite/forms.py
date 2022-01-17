45f4da87f0385965b5744f2329dbe090f9a43f64
JordanCH05 <jordi.caristiahill@gmail.com>
2022-01-17 16:54:18 +0000

    Add required=True to score

--- reviewsite/forms.py
+++ reviewsite/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 
 class ReviewForm(forms.ModelForm):
-    score = forms.DecimalField(decimal_places=2, max_digits=3, min_value=0, max_value=5)
+    score = forms.DecimalField(decimal_places=2, max_digits=3, min_value=0, max_value=5, required=True)
 
     class Meta:
         model = Review
