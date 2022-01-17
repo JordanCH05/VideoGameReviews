1017792a28b404b3649e147785da44670b5d3c68
JordanCH05 <jordi.caristiahill@gmail.com>
2022-01-17 13:14:41 +0000

    Add game_detail path to urls

--- reviewsite/urls.py
+++ reviewsite/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.GameList.as_view(), name='home')
+    path('', views.GameList.as_view(), name='home'),
+    path('<slug:slug>/', views.GameDetail.as_view(), name='game_detail'),
 ]
