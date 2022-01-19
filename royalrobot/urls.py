0bfdcac204514db474fc84328616dfb470ce50bc
JordanCH05 <jordi.caristiahill@gmail.com>
2022-01-19 13:05:24 +0000

    Add AllAuth to settings and urls

--- royalrobot/urls.py
+++ royalrobot/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('reviewsite.urls'), name='reviewsite_urls')
+    path('', include('reviewsite.urls'), name='reviewsite_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
