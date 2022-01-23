f3aedaea6075dfa09fba80382565e575a2986208
JordanCH05 <jordi.caristiahill@gmail.com>
2022-01-23 16:32:02 +0000

    Add stars to Review Form

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', starRating);
+
+document.querySelector("#id_score").addEventListener('input', starReviewRating)
     
 /**
 * Styles star rating width based on score
@@ -16,6 +18,27 @@ function starRating() {
     
 }
 
+/**
+* Styles star rating width based on score input
+    */
+function starReviewRating() {
+
+    const score_input = document.querySelector("#id_score").value;
+    const star_ratings = document.querySelectorAll(".stars-inner");
+    const star_rating = star_ratings[star_ratings.length - 1];
+    if (score_input >= 0 && score_input <= 5) {
+        const ratingPercent = (score_input / 5) * 100;
+        const ratingPercentRounded = `${Math.round
+            ((ratingPercent) / 10) * 10}%`;
+        star_rating.style.width = ratingPercentRounded;
+    }
+    
+    console.log(ratingPercent);
+}
+
+/**
+* Times out messages to close after a few seconds
+    */
 setTimeout(function () {
     let messages = document.getElementById('msg');
     let alert = new bootstrap.Alert(messages);
