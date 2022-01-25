d52b3f2597247cf8db9e64db4c17573a58fe0aa4
JordanCH05 <jordi.caristiahill@gmail.com>
2022-01-25 11:52:16 +0000

    Break long lines into 2 PEP8

--- reviewsite/urls.py
+++ reviewsite/urls.py
@@ -5,6 +5,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.GameList.as_view(), name='home'),
     path('<slug:slug>/', views.GameDetail.as_view(), name='game_detail'),
-    path('edit_review/<review_id>/<slug>', views.edit_review, name='edit_review'),
-    path('delete_review/<review_id>/<slug>', views.delete_review, name='delete_review'),
+    path('edit_review/<review_id>/<slug>', views.edit_review,
+         name='edit_review'),
+    path('delete_review/<review_id>/<slug>', views.delete_review,
+         name='delete_review'),
 ]
